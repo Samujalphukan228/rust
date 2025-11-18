161c157d88e053e9ba4562f987e305c029692ea0
Your Name <your.email@example.com>
2025-11-18 12:30:35 +0530

    3

---
+++ rust-learning/Variables/src/main.rs
@@ -0,0 +1,18 @@
+let name = "Samujal phukan";
+println!("My name is: {}", name);
+
+
+// let name = "John";
+// let age = 30;
+// println!("{} is {} years old.", age, name);  
+
+
+// let x = 5;
+// x = 10; // Error
+// println!("{}", x);
+
+
+// let mut x = 5;
+// println!("Before: {}", x);
+// x = 10;
+// println!("After: {}", x);
