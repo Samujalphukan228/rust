aa6061b02848855d4971cfcb081a334c617ca8d0
Your Name <your.email@example.com>
2025-11-18 13:03:36 +0530

    5

---
+++ rust-learning/Booleans/src/main.rs
@@ -0,0 +1,25 @@
+// Boolean Variables
+
+// Example
+let is_programming_fun: bool = true;
+let is_fish_tasty: bool = false;
+
+println!("Is Programming Fun? {}", is_programming_fun);
+println!("Is Fish Tasty? {}", is_fish_tasty);
+
+// Boolean from Comparison
+// Example
+let age = 20;
+let can_vote = age >= 18;
+
+println!("Can vote? {}", can_vote);
+
+// Booleans in if Statements
+//Example
+let is_logged_in = true;
+
+if is_logged_in {
+    println!("Welcome back!");
+} else {
+    println!("Please log in.");
+}
