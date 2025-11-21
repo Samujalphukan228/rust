b24070c7f370183514d8583ebb9c0e80391283c5
Your Name <your.email@example.com>
2025-11-21 07:03:54 +0530

    3

---
+++ rust-learning/vector/src/main.rs
@@ -0,0 +1,40 @@
+// Vector
+
+// Creating a Vector
+let fruits = vec!["apple", "banana", "orange"];
+
+// Access Vector Elements
+// Example
+let fruits = vec!["apple", "banana", "orange"];
+println!("First fruit: {}", fruits[0]);
+
+// Change Vector Values
+// Example
+let mut fruits = vec!["apple", "banana", "orange"];
+fruits[0] = "grape";
+println!("New first fruit: {}", fruits[0]);
+
+// Add Elements to a Vector
+// Example
+let mut fruits = vec!["apple", "banana"];
+fruits.push("cherry");
+println!("{:?}", fruits); 
+
+// Remove Elements from a Vector
+// Example
+let mut fruits = vec!["apple", "banana", "cherry"];
+fruits.pop();
+println!("{:?}", fruits); 
+
+// Add or Remove Elements at a Specified Index
+// Example
+let mut fruits = vec!["banana", "orange"];
+fruits.insert(0, "apple");
+println!("{:?}", fruits);
+
+// Loop Through a Vector
+// Example
+let fruits = vec!["apple", "banana", "orange"];
+for fruit in &fruits {
+  println!("I like {}.", fruit);
+}
